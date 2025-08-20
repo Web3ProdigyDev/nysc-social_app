227d46b11b8a8830dff8664aef14cbe2db9e48cd
Godwin Adakonye John <gj09042003@gmail.com>
2025-08-20 21:58:32 +0100

    App fetch friend and profile posts and App like functinality with useOptimistic hook

--- src/app/components/feed/Post.tsx
+++ src/app/components/feed/Post.tsx
@@ -1,89 +1,66 @@
-import Image from "next/image"
+import Image from "next/image";
 import Comments from "./Comments";
+import { Post as PostType, User } from "@/generated/prisma";
+import PostInteraction from "./PostInteraction";
+import { Suspense } from "react";
+import PostInfo from "./PostInfo";
+import { auth } from "@clerk/nextjs/server";
 
-const Post = () => {
-    return (
-        <div className="flex flex-col gap-4">
-            {/* USER */}
-            <div className="flex items-center justify-between">
-                <div className="flex items-center gap-4">
-                    <Image
-                        src="https://images.pexels.com/photos/32360758/pexels-photo-32360758.jpeg"
-                        width={40}
-                        height={40}
-                        alt=""
-                        className="w-10 h-10 rounded-full"
-                    />
-                    <span className="font-medium">Joseph Jombuti</span>
-                </div>
-                <Image
-                    src="/more.png"
-                    width={16}
-                    height={16}
-                    alt=""
-                />
-            </div>
-            {/* DESCRIPTION */}
-            <div className="flex flex-col gap-4">
-                <div className="w-full min-h-96 relative">
-                    <Image
-                        src="https://images.pexels.com/photos/33151415/pexels-photo-33151415.jpeg"
-                        fill
-                        className="object-cover rounded-md"
-                        alt=""
-                    />
-                </div>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero, voluptatum ut. Reiciendis excepturi libero quam aspernatur maiores, quidem ipsam illum nam vitae ducimus, quibusdam veniam repellat laborum est a eligendi.</p>
-            </div>
-            {/* INTERACTION */}
-            <div className="flex items-center justify-between text-sm my-4">
-                <div className="flex gap-8">
-                    <div className="flex items-center gap-4 bg-slate-100 p-2 rounded-xl">
-                        <Image
-                            src="/like.png"
-                            width={16}
-                            height={16}
-                            className="cursor-pointer"
-                            alt=""
-                        />
-                        <span className="text-gray-300">Like</span>
-                        <span className="text-gray-500">123
-                            <span className="hidden md:inline"> Likes</span>
-                        </span>
-                    </div>
-                    <div className="flex items-center gap-4 bg-slate-100 p-2 rounded-xl">
-                        <Image
-                            src="/comment.png"
-                            width={16}
-                            height={16}
-                            className="cursor-pointer"
-                            alt=""
-                        />
-                        <span className="text-gray-300"></span>
-                        <span className="text-gray-500">123
-                            <span className="hidden md:inline"> Comments</span>
-                        </span>
-                    </div>
-                </div>
-                <div className="">
-                    <div className="flex items-center gap-4 bg-slate-100 p-2 rounded-xl">
-                        <Image
-                            src="/share.png"
-                            width={16}
-                            height={16}
-                            className="cursor-pointer"
-                            alt=""
-                        />
-                        <span className="text-gray-300"></span>
-                        <span className="text-gray-500">123
-                            <span className="hidden md:inline"> Shares</span>
-                        </span>
-                    </div>
-                </div>
-            </div>
-            <Comments />
-        </div >
-    );
+type FeedPostType = PostType & { user: User } & {
+  likes: [{ userId: string }];
+} & {
+  _count: { comments: number };
 };
 
-export default Post
+const Post = ({ post }: { post: FeedPostType }) => {
+  const { userId } = auth();
+  return (
+    <div className="flex flex-col gap-4">
+      {/* USER */}
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <Image
+            src={post.user.avatar || "/noAvatar.png"}
+            width={40}
+            height={40}
+            alt=""
+            className="w-10 h-10 rounded-full"
+          />
+          <span className="font-medium">
+            {post.user.name && post.user.surname
+              ? post.user.name + " " + post.user.surname
+              : post.user.username}
+          </span>
+        </div>
+        {userId === post.user.id && <PostInfo postId={post.id} />}
+      </div>
+      {/* DESC */}
+      <div className="flex flex-col gap-4">
+        {post.img && (
+          <div className="w-full min-h-96 relative">
+            <Image
+              src={post.img}
+              fill
+              className="object-cover rounded-md"
+              alt=""
+            />
+          </div>
+        )}
+        <p>{post.desc}</p>
+      </div>
+      {/* INTERACTION */}
+      <Suspense fallback="Loading...">
+        <PostInteraction
+          postId={post.id}
+          likes={post.likes.map((like) => like.userId)}
+          commentNumber={post._count.comments}
+        />
+      </Suspense>
+      <Suspense fallback="Loading...">
+        <Comments postId={post.id} />
+      </Suspense>
+    </div>
+  );
+};
+
+export default Post;
