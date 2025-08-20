716427c62a19b780f5bc7e2ec71b4a21c6cdb84d
Godwin Adakonye John <gj09042003@gmail.com>
2025-08-20 22:47:27 +0100

    Fixed the Update user error

--- src/app/components/rightMenu/UserInfoCard.tsx
+++ src/app/components/rightMenu/UserInfoCard.tsx
@@ -1,8 +1,6 @@
-"use server";
-
-import { User } from "@/generated/prisma";
 import prisma from "@/lib/client";
 import { auth } from "@clerk/nextjs/server";
+import { User } from "@/generated/prisma";
 import Image from "next/image";
 import Link from "next/link";
 import UserInfoCardInteraction from "./UserInfoCardInteraction";
@@ -10,109 +8,116 @@ import UpdateUser from "./UpdateUser";
 
 const UserInfoCard = async ({ user }: { user: User }) => {
   const createdAtDate = new Date(user.createdAt);
+
   const formattedDate = createdAtDate.toLocaleDateString("en-US", {
-    day: "numeric",
-    month: "long",
     year: "numeric",
+    month: "long",
+    day: "numeric",
   });
 
-  const { userId: currentUserId } = auth();
   let isUserBlocked = false;
   let isFollowing = false;
-  let isFollowingRequestSent = false;
+  let isFollowingSent = false;
+
+  const { userId: currentUserId } = auth();
 
   if (currentUserId) {
     const blockRes = await prisma.block.findFirst({
       where: {
-        blockedId: { equals: currentUserId },
-        blockerId: { equals: user.id },
+        blockerId: currentUserId,
+        blockedId: user.id,
       },
     });
-    isUserBlocked = !!blockRes;
 
+    blockRes ? (isUserBlocked = true) : (isUserBlocked = false);
     const followRes = await prisma.follower.findFirst({
       where: {
-        followerId: { equals: currentUserId },
-        followingId: { equals: user.id },
+        followerId: currentUserId,
+        followingId: user.id,
       },
     });
-    isFollowing = !!followRes;
 
+    followRes ? (isFollowing = true) : (isFollowing = false);
     const followReqRes = await prisma.followRequest.findFirst({
       where: {
-        senderId: { equals: currentUserId },
-        receiverId: { equals: user.id },
+        senderId: currentUserId,
+        receiverId: user.id,
       },
     });
-    isFollowingRequestSent = !!followReqRes;
-  }
 
+    followReqRes ? (isFollowingSent = true) : (isFollowingSent = false);
+  }
   return (
-    <div>
-      <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
-        {/* TOP */}
-        <div className="flex justify-between items-center font-medium">
-          <span className="text-gray-500">User Information</span>
-          { currentUserId === user.id ? (<UpdateUser />) : (<Link href="/" className="text-green-500 text-xs">
+    <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
+      {/* TOP */}
+      <div className="flex justify-between items-center font-medium">
+        <span className="text-gray-500">User Information</span>
+        {currentUserId === user.id ? (
+          <UpdateUser user={user}/>
+        ) : (
+          <Link href="/" className="text-blue-500 text-xs">
             See all
-          </Link>)}
+          </Link>
+        )}
+      </div>
+      {/* BOTTOM */}
+      <div className="flex flex-col gap-4 text-gray-500">
+        <div className="flex items-center gap-2">
+          <span className="text-xl text-black">
+            {" "}
+            {user.name && user.surname
+              ? user.name + " " + user.surname
+              : user.username}
+          </span>
+          <span className="text-sm">@{user.username}</span>
         </div>
-        {/* BOTTOM */}
-        <div className="flex flex-col gap-4 text-gray-500">
+        {user.description && <p>{user.description}</p>}
+        {user.city && (
           <div className="flex items-center gap-2">
-            <span className="text-xl text-black">
-              {(user.name && user.surname) ? `${user.name} ${user.surname}` : user.username}
+            <Image src="/map.png" alt="" width={16} height={16} />
+            <span>
+              Living in <b>{user.city}</b>
             </span>
-            <span className="text-sm">@{user.username}</span>
           </div>
-          {user.description && <p>{user.description}</p>}
-          {user.city && (
-            <div className="flex items-center gap-2">
-              <Image src="/map.png" alt="" width={16} height={16} />
-              <span>
-                Posted to <b>{user.city}</b>
-              </span>
-            </div>
-          )}
-          {user.school && (
-            <div className="flex items-center gap-2">
-              <Image src="/school.png" alt="" width={16} height={16} />
-              <span>
-                Went to <b>{user.school}</b>
-              </span>
-            </div>
-          )}
-          {user.work && (
-            <div className="flex items-center gap-2">
-              <Image src="/work.png" alt="" width={16} height={16} />
-              <span>
-                PPA: <b>{user.work}</b>
-              </span>
-            </div>
-          )}
-          <div className="flex items-center justify-between">
-            {user.website && (
-              <div className="flex gap-1 items-center">
-                <Image src="/link.png" alt="" width={16} height={16} />
-                <Link href={user.website} className="text-green-500 font-medium">
-                  {user.website}
-                </Link>
-              </div>
-            )}
+        )}
+        {user.school && (
+          <div className="flex items-center gap-2">
+            <Image src="/school.png" alt="" width={16} height={16} />
+            <span>
+              Went to <b>{user.school}</b>
+            </span>
+          </div>
+        )}
+        {user.work && (
+          <div className="flex items-center gap-2">
+            <Image src="/work.png" alt="" width={16} height={16} />
+            <span>
+              Works at <b>{user.work}</b>
+            </span>
+          </div>
+        )}
+        <div className="flex items-center justify-between">
+          {user.website && (
             <div className="flex gap-1 items-center">
-              <Image src="/date.png" alt="" width={16} height={16} />
-              <span>Joined {formattedDate}</span>
+              <Image src="/link.png" alt="" width={16} height={16} />
+              <Link href={user.website} className="text-blue-500 font-medium">
+                {user.website}
+              </Link>
             </div>
+          )}
+          <div className="flex gap-1 items-center">
+            <Image src="/date.png" alt="" width={16} height={16} />
+            <span>Joined {formattedDate}</span>
           </div>
-          { currentUserId && currentUserId !== user.id && (
-            <UserInfoCardInteraction
+        </div>
+        {currentUserId && currentUserId !== user.id && (
+          <UserInfoCardInteraction
             userId={user.id}
             isUserBlocked={isUserBlocked}
             isFollowing={isFollowing}
-            isFollowingSent={isFollowingRequestSent}
+            isFollowingSent={isFollowingSent}
           />
         )}
-        </div>
       </div>
     </div>
   );
